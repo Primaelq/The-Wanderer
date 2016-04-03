67ea1bad8be1dea13e8898db07a1656dc9f85087
Primaël Quémerais <primaelq@gmail.com>
2016-04-03 12:02:16 +0100

    New prototyping scenes

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float walkSpeed = 5.0f;
+    public float runSpeed = 10.0f;
+
+	void Start ()
+    {
+	    
+	}
+	
+	void Update ()
+    {
+	    
+	}
+}
