dc2c6f859dd244509a38229ab90034b6d849a088
Primaël Quémerais <primaelq@gmail.com>
2016-04-03 12:13:35 +0100

    PlayerController

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,16 +4,44 @@ using System.Collections;
 public class PlayerController : MonoBehaviour
 {
 
+    public float rotateSpeed = 8.0f;
     public float walkSpeed = 5.0f;
     public float runSpeed = 10.0f;
 
+    private Rigidbody playerRgb; 
+
 	void Start ()
     {
-	    
+        playerRgb = GetComponent<Rigidbody>();
 	}
 	
 	void Update ()
     {
 	    
 	}
+
+    void FixedUpdate()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        
+        Plane floor = new Plane(Vector3.up, transform.position);
+
+        float hitdist = 0.0f;
+
+        if (floor.Raycast(ray, out hitdist))
+        {
+            Vector3 targetPoint = ray.GetPoint(hitdist);
+            
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalInput, 0.0f, verticalInput);
+
+        playerRgb.velocity = movement * walkSpeed;
+    }
 }
