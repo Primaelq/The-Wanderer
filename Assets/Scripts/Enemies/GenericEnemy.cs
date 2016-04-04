f5fa10004cc17db6847b6982baa6683f1a0ac0da
Primaël Quémerais <primaelq@gmail.com>
2016-04-04 22:28:02 +0100

    Fixed Enemies HeatlhBars

--- Assets/Scripts/Enemies/GenericEnemy.cs
+++ Assets/Scripts/Enemies/GenericEnemy.cs
@@ -14,7 +14,8 @@ public class GenericEnemy : MonoBehaviour
 	void Start ()
     {
         indicator.SetActive(false);
-	}
+        healthBar.transform.position = new Vector3(background.transform.position.x, background.transform.position.y, background.transform.position.z - 0.0001f);
+    }
 	
 	void Update ()
     {
@@ -37,6 +38,6 @@ public class GenericEnemy : MonoBehaviour
         healthBar.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
         background.transform.rotation = Quaternion.LookRotation(Camera.main.transform.forward);
 
-        healthBar.transform.position = new Vector3(background.transform.position.x - ((1 - healthBar.transform.localScale.x) / 2), background.transform.position.y, background.transform.position.z);
+        healthBar.transform.position = new Vector3(background.transform.position.x - ((1 - healthBar.transform.localScale.x) / 2), background.transform.position.y, background.transform.position.z - 0.0001f);
     }
 }
