5f477dbf48acd35099d20d94f2c4ebf94b4f5961
Primaël Quémerais <primaelq@gmail.com>
2016-04-04 23:17:30 +0100

    Zombies Attack Fixed

--- Assets/Scripts/Zombies/ZombieController.cs
+++ Assets/Scripts/Zombies/ZombieController.cs
@@ -9,18 +9,47 @@ public class ZombieController : MonoBehaviour
     public float hitDistance = 1.0f;
     public int damages = 5;
 
-	void Start ()
+    public float hitSpeed = 2.0f;
+
+    private float startSpeed;
+    private bool attackReady = false;
+
+    void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-	}
-	
-	void Update ()
+        startSpeed = agent.speed;
+        StartCoroutine(WaitBeforeStart());
+    }
+
+    void Update()
     {
         agent.SetDestination(target.position);
 
-        if(agent.remainingDistance <= hitDistance)
+        if (agent.remainingDistance <= hitDistance && attackReady)
         {
             target.GetComponent<PlayerGUI>().currentHealth -= damages;
+            attackReady = false;
+            StartCoroutine(AttackDelay());
         }
-	}
+        else if (agent.remainingDistance <= hitDistance)
+        {
+            agent.speed = 0;
+        }
+        else
+        {
+            agent.speed = startSpeed;
+        }
+    }
+
+    IEnumerator AttackDelay()
+    {
+        yield return new WaitForSeconds(hitSpeed);
+        attackReady = true;
+    }
+
+    IEnumerator WaitBeforeStart()
+    {
+        yield return new WaitForSeconds(1);
+        attackReady = true;
+    }
 }
