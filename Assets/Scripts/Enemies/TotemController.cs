5b9ad2fe678231451a1890c467750867e163a8ee
Primaël Quémerais <primaelq@gmail.com>
2016-04-06 15:32:36 +0100

    New Projectile System

--- Assets/Scripts/Enemies/TotemController.cs
+++ Assets/Scripts/Enemies/TotemController.cs
@@ -3,18 +3,34 @@ using System.Collections;
 
 public class TotemController : MonoBehaviour 
 {
-	public Transform cannonBall_Prefab;
-	public Transform cannonEnd;
+	public GameObject bullet;
 	public float fireRatePerSec = 1; //Number of shots per second
+    public float scope = 8.0f;
+    public int damages = 5;
 
-	// Use this for initialization
 	void Start () 
 	{
 		InvokeRepeating("Shoot", 0.0f, fireRatePerSec);
 	}
 	
 	public void Shoot()
-	{
-		Instantiate(cannonBall_Prefab, cannonEnd.position, cannonEnd.rotation);
+    {
+        StartCoroutine(Delay(0.1f));
+
+        RaycastHit hit;
+
+        if(Physics.Raycast(transform.position, -transform.forward, out hit) && hit.transform.tag == "Player" && hit.distance <= scope)
+        {
+            hit.transform.GetComponent<PlayerHelper>().currentHealth -= damages;
+        }
 	}
+
+    IEnumerator Delay(float waitTime)
+    {
+        var em = bullet.GetComponent<ParticleSystem>().emission;
+
+        em.enabled = true;
+        yield return new WaitForSeconds(waitTime);
+        em.enabled = false;
+    }
 }
