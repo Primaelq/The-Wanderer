a24b4519e26ff33ebbcbacd88ce170296f9ea71b
superobot1 <dvd121200@walla.com>
2016-04-07 19:48:09 +0300

    Added Generic Enemy(Unfinished)

---
+++ Assets/Scripts/GenericEntity.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericEntity : MonoBehaviour 
+{
+	//TODO: think about adding damage
+	public float health;
+	public float baseSpeed;
+	public float currentSpeed;
+
+	[HideInInspector]
+	public bool canAttack = true;
+
+	
+
+}
