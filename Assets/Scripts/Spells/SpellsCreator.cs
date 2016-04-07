8226efc0493ab7e3b4311903f4e5ee08e084d283
Primaël Quémerais <primaelq@gmail.com>
2016-04-07 14:35:27 +0100

    New SpellsCreator

--- Assets/Scripts/Spells/SpellsCreator.cs
+++ Assets/Scripts/Spells/SpellsCreator.cs
@@ -8,7 +8,7 @@ public class SpellsCreator : EditorWindow
 
     Sprite icon;
 
-    string[] types = {"Attack", "Treat", "Invocation"};
+    string[] types = { "Attack", "Treat", "Invocation" };
 
     int damages, health, type;
 
@@ -20,6 +20,8 @@ public class SpellsCreator : EditorWindow
 
     Animation playerAnimLoading, playerAnimLauching;
 
+    GameObject spellObject; //Will be null only if this spell wasn't created yet
+
     [MenuItem("Window/Spells Creator")]
     public static void ShowWindow()
     {
@@ -29,13 +31,15 @@ public class SpellsCreator : EditorWindow
     void OnGUI()
     {
         EditorGUILayout.Separator();
+        EditorGUILayout.LabelField("Editing existing asset: " + (spellObject != null));
+        EditorGUILayout.Separator();
         spellName = EditorGUILayout.TextField("Name", spellName);
         EditorGUILayout.Separator();
         description = EditorGUILayout.TextField("Description", description);
         EditorGUILayout.Separator();
         icon = (Sprite)EditorGUILayout.ObjectField("Icon", icon, typeof(Sprite), false);
         EditorGUILayout.Separator();
-        
+
         type = EditorGUILayout.Popup("Type", type, types);
 
         EditorGUI.indentLevel++;
@@ -54,15 +58,15 @@ public class SpellsCreator : EditorWindow
         EditorGUI.indentLevel--;
 
         EditorGUILayout.Separator();
-        if(zone = EditorGUILayout.Toggle("Zone", zone))
+        if (zone = EditorGUILayout.Toggle("Zone", zone))
         {
             EditorGUI.indentLevel++;
             radius = EditorGUILayout.FloatField("Radius", radius);
-            if(type == 0)
+            if (type == 0)
             {
                 divideDamages = EditorGUILayout.Toggle("Divide Damages", divideDamages);
             }
-            else if(type == 1)
+            else if (type == 1)
             {
                 divideHealth = EditorGUILayout.Toggle("Divide Health", divideHealth);
             }
@@ -82,15 +86,101 @@ public class SpellsCreator : EditorWindow
         playerAnimLauching = (Animation)EditorGUILayout.ObjectField("Launching", playerAnimLauching, typeof(Animation), false);
         EditorGUI.indentLevel--;
         GUILayout.Space(50);
-        if (GUILayout.Button("Create"))
+
+        if (GUILayout.Button("Save"))
         {
-            GameObject tempSpell = new GameObject();
-            tempSpell.AddComponent<SpellTemplate>();
+            SaveSpell();
+        }
+        if (GUILayout.Button("New Spell Template"))
+        {
+            NewSpell();
+        }
+        if (GUILayout.Button("Load Spell Template"))
+        {
+            LoadSpell();
+        }
+    }
+
+    private void LoadSpell()
+    {
+        string absPath = EditorUtility.OpenFilePanel("Select Spell Template ", "Assets/Prefabs/Spells/", "");
+        if (absPath.StartsWith(Application.dataPath))
+        {
+            string relPath = absPath.Substring(Application.dataPath.Length - "Assets".Length);
+            GameObject tempSpell = AssetDatabase.LoadAssetAtPath(relPath, typeof(GameObject)) as GameObject;
+            SpellTemplate spellt = tempSpell.GetComponent<SpellTemplate>();
+            if (spellt != null)
+            {
+                //TODO: add divideHealth, and stopPlayerWhenCasting
+                spellObject = tempSpell;
+                spellName = spellt.spellName;
+                description = spellt.description;
+                icon = spellt.icon;
+                type = spellt.type;
+                damages = spellt.damages;
+                health = spellt.health;
+                zone = spellt.zone;
+                divideDamages = spellt.divideDamages;
+                radius = spellt.radius;
+                castTime = spellt.castTime;
+                rechargeTime = spellt.rechargeTime;
+                prefab = spellt.prefab;
+                particleEffect = spellt.particleEffect;
+                playerAnimLoading = spellt.loading;
+                playerAnimLauching = spellt.launching;
+            }
+            else
+            {
+                Debug.LogError("Selected prefab didn't have a 'spell template' script attached to them");
+            }
+        }
+    }
+
+    private void NewSpell()
+    {
+        //New spell
+        spellObject = null;
+        //Resetting editor
+        spellName = "";
+        description = "";
+        icon = null;
+        type = 0;
+        damages = 0;
+        health = 0;
+        zone = false;
+        divideHealth = false;
+        divideDamages = false;
+        radius = 0.0f;
+        castTime = 0.0f;
+        rechargeTime = 0.0f;
+        stopPlayerWhenCasting = false;
+        prefab = null;
+        particleEffect = null;
+        playerAnimLoading = null;
+        playerAnimLauching = null;
+    }
+
+    private void SaveSpell()
+    {
+        if (spellObject == null)
+        {
+            Debug.Log("Creating this as a new spell since it was never saved before");
+            CreateSpell();
+        }
+        else
+        {
+            if (spellObject.name != spellName)
+            {
+                string relPath = AssetDatabase.GetAssetPath(spellObject);
+                AssetDatabase.RenameAsset(relPath, spellName);
+            }
+            Debug.Log("Saving spell!");
+            GameObject tempSpell = spellObject;
             tempSpell.GetComponent<SpellTemplate>().spellName = spellName;
             tempSpell.GetComponent<SpellTemplate>().description = description;
             tempSpell.GetComponent<SpellTemplate>().icon = icon;
             tempSpell.GetComponent<SpellTemplate>().type = type;
-            switch(type)
+            switch (type)
             {
                 case 0:
                     tempSpell.GetComponent<SpellTemplate>().damages = damages;
@@ -103,7 +193,7 @@ public class SpellsCreator : EditorWindow
                     break;
             }
             tempSpell.GetComponent<SpellTemplate>().zone = zone;
-            if(zone)
+            if (zone)
             {
                 tempSpell.GetComponent<SpellTemplate>().radius = radius;
                 tempSpell.GetComponent<SpellTemplate>().divideDamages = divideDamages;
@@ -113,8 +203,45 @@ public class SpellsCreator : EditorWindow
             tempSpell.GetComponent<SpellTemplate>().particleEffect = particleEffect;
             tempSpell.GetComponent<SpellTemplate>().loading = playerAnimLoading;
             tempSpell.GetComponent<SpellTemplate>().launching = playerAnimLauching;
-
-            GameObject newSpell = PrefabUtility.CreatePrefab("Assets/Prefabs/Spells/" + spellName + ".prefab", tempSpell);
         }
     }
+
+    private void CreateSpell()
+    {
+        GameObject tempSpell = new GameObject();
+        tempSpell.AddComponent<SpellTemplate>();
+        tempSpell.GetComponent<SpellTemplate>().spellName = spellName;
+        tempSpell.GetComponent<SpellTemplate>().description = description;
+        tempSpell.GetComponent<SpellTemplate>().icon = icon;
+        tempSpell.GetComponent<SpellTemplate>().type = type;
+        switch (type)
+        {
+            case 0:
+                tempSpell.GetComponent<SpellTemplate>().damages = damages;
+                break;
+            case 1:
+                tempSpell.GetComponent<SpellTemplate>().health = health;
+                break;
+            case 2:
+                tempSpell.GetComponent<SpellTemplate>().prefab = prefab;
+                break;
+        }
+        tempSpell.GetComponent<SpellTemplate>().zone = zone;
+        if (zone)
+        {
+            tempSpell.GetComponent<SpellTemplate>().radius = radius;
+            tempSpell.GetComponent<SpellTemplate>().divideDamages = divideDamages;
+        }
+        tempSpell.GetComponent<SpellTemplate>().castTime = castTime;
+        tempSpell.GetComponent<SpellTemplate>().rechargeTime = rechargeTime;
+        tempSpell.GetComponent<SpellTemplate>().particleEffect = particleEffect;
+        tempSpell.GetComponent<SpellTemplate>().loading = playerAnimLoading;
+        tempSpell.GetComponent<SpellTemplate>().launching = playerAnimLauching;
+
+        GameObject newSpell = PrefabUtility.CreatePrefab("Assets/Prefabs/Spells/" + spellName + ".prefab", tempSpell);
+        //string relPath = AssetDatabase.GetAssetPath(newSpell);
+        spellObject = newSpell;
+        DestroyImmediate(tempSpell);
+    }
 }
+
