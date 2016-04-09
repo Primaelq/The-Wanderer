1520068e8d485f2a3c362953b95e53bab6d14645
superobot1 <dvd121200@walla.com>
2016-04-09 21:54:25 +0300

    Added timer system. Needs to be checked and perfected.

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Timer
+{
+	//public int importance;
+	public float timerTime; //The time until the timer ends
+	public float timerCounter; //The counter that counts until the timer ends
+
+	//This method is used for counting. It should be called every frame.
+	public void UpdateTimer()
+	{
+		if(timerTime > 0) //If you need to count
+		{
+			if(timerCounter < timerTime)
+			{
+				timerCounter += Time.deltaTime;
+			}
+			else
+			{
+				//Stop counting
+				timerTime = 0;
+				timerCounter = 0;
+			}
+		}
+	}
+
+	//A method to check if the timer is still counting
+	public bool CheckEnd()
+	{
+		if(timerTime == 0)
+			return true;
+		return false;
+	}
+}
