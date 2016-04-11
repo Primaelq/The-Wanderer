e5b460b0816dd920504d88e0de619269f0274801
Primaël Quémerais <primaelq@gmail.com>
2016-04-11 21:57:08 +0100

    Fixed PanelSwitch (MainMenu)

--- Assets/Scripts/Menu/PanelsManager.cs
+++ Assets/Scripts/Menu/PanelsManager.cs
@@ -10,22 +10,24 @@ public class PanelsManager : MonoBehaviour
     public float offset;
 
     private int currentPanel = 0;
+
+    private Vector3 enabledPosition;
     
 	void Start ()
     {
-
-	}
+        enabledPosition = panels[0].GetComponent<RectTransform>().anchoredPosition;
+    }
 	
 	void Update ()
     {
-	
+	    
 	}
 
     public void SwitchPanel(int index)
     {
-        panels[currentPanel].GetComponent<RectTransform>().anchoredPosition = new Vector3(offset, 0.0f, 0.0f);
-            
-        panels[index].GetComponent<RectTransform>().anchoredPosition = new Vector3(8, 0.0f, 0.0f);
+        panels[currentPanel].GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(panels[currentPanel].GetComponent<RectTransform>().anchoredPosition, new Vector3(offset, 0.0f, 0.0f), 0.8f);
+
+        panels[index].GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(panels[index].GetComponent<RectTransform>().anchoredPosition, enabledPosition, 0.8f);
 
         currentPanel = index;
     }
