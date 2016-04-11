55428e359abd429536300845e7363985972e1da8
Primaël Quémerais <primaelq@gmail.com>
2016-04-11 23:03:35 +0100

    Animated MainMenu

--- Assets/Scripts/Menu/PanelsManager.cs
+++ Assets/Scripts/Menu/PanelsManager.cs
@@ -7,28 +7,67 @@ public class PanelsManager : MonoBehaviour
 {
     public List<GameObject> panels;
 
+    public GameObject planet;
+
     public float offset;
 
     private int currentPanel = 0;
 
     private Vector3 enabledPosition;
+    private Vector3 planetStartPos;
     
 	void Start ()
     {
         enabledPosition = panels[0].GetComponent<RectTransform>().anchoredPosition;
+
+        planetStartPos = planet.transform.position;
     }
 	
 	void Update ()
     {
-	    
+
 	}
 
     public void SwitchPanel(int index)
     {
-        panels[currentPanel].GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(panels[currentPanel].GetComponent<RectTransform>().anchoredPosition, new Vector3(offset, 0.0f, 0.0f), 0.8f);
+        if(index != currentPanel)
+        {
+            StartCoroutine(Slide(currentPanel, new Vector3(offset, 0.0f, 0.0f), 1.5f));
 
-        panels[index].GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(panels[index].GetComponent<RectTransform>().anchoredPosition, enabledPosition, 0.8f);
+            if (currentPanel == 0)
+            {
+                StartCoroutine(MovePlanet(new Vector3(20.0f, planetStartPos.y, 0.0f), 0.5f));
+            }
+            else if (index == 0)
+            {
+                StartCoroutine(MovePlanet(planetStartPos, 1.5f));
+            }
 
-        currentPanel = index;
+            StartCoroutine(Slide(index, enabledPosition, 1.5f));
+
+            currentPanel = index;
+        }
+    }
+
+    IEnumerator Slide(int index, Vector3 target, float overTime)
+    {
+        float startTime = Time.time;
+        while (Time.time < startTime + overTime)
+        {
+            panels[index].GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(panels[index].GetComponent<RectTransform>().anchoredPosition, target, (Time.time - startTime) / overTime);
+            yield return null;
+        }
+        panels[index].GetComponent<RectTransform>().anchoredPosition = target;
+    }
+
+    IEnumerator MovePlanet(Vector3 target, float overTime)
+    {
+        float startTime = Time.time;
+        while (Time.time < startTime + overTime)
+        {
+            planet.transform.position = Vector3.Lerp(planet.transform.position, target, (Time.time - startTime) / overTime);
+            yield return null;
+        }
+        planet.transform.position = target;
     }
 }
---
+++ Assets/Scripts/Menu/PlanetController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetController : MonoBehaviour
+{
+    public float sensitivity = 5.0f;
+
+    private Vector2 movement;
+
+    private bool idle = true;
+
+	void Start ()
+    {
+	
+	}
+	
+	void Update ()
+    {
+        movement.x = Input.GetAxis("Vertical") * sensitivity;
+        movement.y = Input.GetAxis("Horizontal") * sensitivity;
+
+        transform.Rotate(movement);
+        
+        if (movement.x != 0.0f || movement.y != 0.0f)
+        {
+            idle = false;
+        }
+        else if (idle)
+        {
+            transform.Rotate(Vector3.up * sensitivity / 5);
+        }
+	}
+}
